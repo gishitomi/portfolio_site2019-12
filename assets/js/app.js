aef4585945f7f08bc0ecc90c65cddf4972b6b794
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-01 02:17:05 +0900

    ポートフォリオサイト新規

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+// console.log(123);
+
+$(function () {
+    // $('#izena').hide();
+    setInterval(function () {
+        $('#izena').fadeIn(3000).delay(15000).fadeOut(3000).delay(15000);
+        $('#sea').delay(2500).fadeIn(3000).delay(7000).fadeOut(3000).delay(3500);
+        $('#hibiscus').delay(3500).fadeIn(3000).delay(7000).fadeOut(3000).delay(4500);
+        $('#aerial').delay(4500).fadeIn(3000).delay(7000).fadeOut(3000).delay(5500);
+        $('#yuhi').delay(5500).fadeIn(3000).delay(7000).fadeOut(3000).delay(6500);
+
+
+
+    });
+
+
+
+
+});
+
+
