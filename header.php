cae40d7210cbcbfc2ed024b2bbf28b10f85371e1
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-05 02:04:43 +0900

    no message

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     </div>
     <nav>
         <li><a href="./about.php">About</a></li>
-        <li><a href="./portfolio.html">Portfolio</a></li>
+        <li><a href="./portfolio.php">Portfolio</a></li>
         <li><a href="./contact/index.php">Contact</a></li>
     </nav>
 </header>
