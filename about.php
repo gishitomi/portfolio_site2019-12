fd0ef8bb0bde8242d02938591c488ff8edc45a5a
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-05 01:47:22 +0900

    ボタンの追加

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/about.css">
+</head>
+
+<body>
+    <div class="container-wrapper1">
+        <div class="container">
+            <header>
+                <div class="header-left">
+                    <h1><a href="./index.html">Shota Gishitomi</a></h1>
+                </div>
+                <nav>
+                    <li><a href="./about.html">About</a></li>
+                    <li><a href="./portfolio.html">Portfolio</a></li>
+                    <li><a href="">Contact</a></li>
+                </nav>
+            </header>
+            <h2>~Introduction~</h2>
+            <main>
+                <div class="left-text">
+                    <p>現役の大学生。３ヶ月間プログラミングスクールにて<br>HTML・CSSコーディング、JavaScriptやPHP等を勉強。<br>現在Webエンジニアを目指し就職活動中。</p>
+                    <img src="./assets/img/blogging-336376_1920.jpg" alt="">
+                </div>
+                <div class="right-text">
+                    <h3>~ Profile ~</h3>
+                    <dl>
+                        <dt>名前 : </dt>
+                        <dd>宜志富紹太 (ギシトミショウタ)</dd>
+                        <dt>出身地 : </dt>
+                        <dd>沖縄県</dd>
+                        <dt>メールアドレス : </dt>
+                        <dd>メールアドレス</dd>
+                        <dt>Facebook : </dt>
+                        <dd>アカウント名</dd>
+                        <dt>Instagram : </dt>
+                        <dd>アカウント名</dd>
+                    </dl>
+                </div>
+            </main>
+        </div>
+    </div>
+    <div class="space">
+        <h2>About</h2>
+    </div>
+    <div class="container-wrapper2">
+        <div class="container2">
+
+        </div>
+    </div>
+
+</body>
+
+</html>
