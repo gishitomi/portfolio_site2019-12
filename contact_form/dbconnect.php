7f60e7e43ea4323fe082e096dc7876de58a0aacb
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-15 14:26:26 +0900

    no message

--- contact_form/dbconnect.php
+++ contact_form/dbconnect.php
@@ -8,12 +8,11 @@
 // $user = 'root'; //MySQLにログインするユーザー名
 // $password = ''; //ユーザーのパスワード
 
-$host = "HOSTNAME"; //MySQLがインストールされてるコンピュータ
-$dbname = "DBNAME"; //使用するDB
+$host = getenv('HOSTNAME'); //MySQLがインストールされてるコンピュータ
+$dbname = getenv('DBNAME'); //使用するDB
 $charset = "utf8"; //文字コード
-$user = 'USERNAME'; //MySQLにログインするユーザー名
-$password = 'PASSWORD'; //ユーザーのパスワード
-
+$user = getenv('USERNAME'); //MySQLにログインするユーザー名
+$password = getenv('PASSWORD'); //ユーザーのパスワード
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
