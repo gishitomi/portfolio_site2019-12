b1e4b7a6666032e451dcceb210cc5b06a302c4e8
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-17 01:48:11 +0900

    no message

--- contact_form/notify_slack.sh
+++ contact_form/notify_slack.sh
@@ -1,7 +1,7 @@
 # ターミナルから実行するやつ
 # sh (shellの略)
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"届いて欲しい"}' https://hooks.slack.com/services/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
 
 # curl -X POST -H 'Content-type: application/json' --data '{
 #     "text": 
