96943a7e8187ef7f6ab4e3c297da539622871459
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-17 01:45:00 +0900

    slackの連携テスト

--- contact_form/notify_slack.sh
+++ contact_form/notify_slack.sh
@@ -3,12 +3,12 @@
 
 # curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
 
-curl -X POST -H 'Content-type: application/json' --data "{
-    'text': 
+curl -X POST -H 'Content-type: application/json' --data '{
+    "text": 
     '
     [問い合わせがきました]
     [名前]$1
     [メールアドレス]$2
     [コメント]$3
     '
-}" $4
+}' $4
