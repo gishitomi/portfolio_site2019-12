b78a224e28a12f22592e427beafa9c90294da576
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-18 19:10:52 +0900

    index.phpのレスポンシブ対応

--- index-link.php
+++ index-link.php
@@ -19,6 +19,14 @@
             <div class="container">
                 <!-- php用 headerファイル -->
                 <?php include('header.php'); ?>
+                <nav class="berger-menu">
+                    <ul>
+                        <li><a href="./index-link.php">Top</a></li>
+                        <li><a href="./about.php">About</a></li>
+                        <li><a href="./portfolio.php">Portfolio</a></li>
+                        <li><a href="./contact_form/index.php">Contact</a></li>
+                    </ul>
+                </nav>
                 <main>
                     <div class="text-box">
                         <p id="message">初めまして！宜志富紹太と申します。<br>私は現在大学に通いつつプログラミング、<br>Web制作等の勉強をしています。<br> 質問やご意見などございましたら
@@ -26,42 +34,45 @@
                         </p>
 
                     </div>
-                            <section>
-                                <div class="button">
-                                    <a href="./portfolio.php" class="twitter-follow-button" data-show-count="false" data-size="large">Shota's Portfolio</a>
-                                    <script>
-                                        ! function(d, s, id) {
-                                            var js, fjs = d.getElementsByTagName(s)[0];
-                                            if (!d.getElementById(id)) {
-                                                js = d.createElement(s);
-                                                js.id = id;
-                                                js.src = "//platform.twitter.com/widgets.js";
-                                                fjs.parentNode.insertBefore(js, fjs);
-                                            }
-                                        }(document, "script", "twitter-wjs");
-                                    </script>
-                                </div>
-                                <div class="cover">
-                                    <div class="innie">
-    
-                                    </div>
-                                    <div class="spine"></div>
-                                    <div class="outie">
-                                        <p>Main skills used</p>
-                                        <p>
-                                            <i class="fab fa-html5"></i>
-                                            <i class="fab fa-css3-alt"></i>
-                                            <i class="fab fa-js"></i>
-                                            <i class="fab fa-php"></i>
-                                            <i class="fab fa-sass"></i>
-                                            <i class="fab fa-github"></i>
-    
-    
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="shadow"></div>
-                            </section>
+                    <section>
+                        <div class="button">
+                            <a href="./portfolio.php" class="twitter-follow-button" data-show-count="false" data-size="large">Shota's Portfolio</a>
+                            <script>
+                                ! function(d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (!d.getElementById(id)) {
+                                        js = d.createElement(s);
+                                        js.id = id;
+                                        js.src = "//platform.twitter.com/widgets.js";
+                                        fjs.parentNode.insertBefore(js, fjs);
+                                    }
+                                }(document, "script", "twitter-wjs");
+                            </script>
+                        </div>
+                        <div class="cover">
+                            <div class="innie">
+
+                            </div>
+                            <div class="spine"></div>
+                            <div class="outie">
+                                <p>Main skills used</p>
+                                <p>
+                                    <i class="fab fa-html5"></i>
+                                    <i class="fab fa-css3-alt"></i>
+                                    <i class="fab fa-js"></i>
+                                    <i class="fab fa-php"></i>
+                                    <i class="fab fa-sass"></i>
+                                    <i class="fab fa-github"></i>
+
+
+                                </p>
+                            </div>
+                        </div>
+                        <div class="shadow"></div>
+                    </section>
+                    <article>
+                        <a href="./portfolio.php" class="btn-text-3d">Shota's Portfolio</a>
+                    </article>
                     <div class="fluid-box">
                         <div class="stretch">
                             <div class="fluid rotate opacity"></div>
@@ -92,6 +103,7 @@
     <script src="./assets/js/header.js"></script>
     <script src="./assets/js/onload.js"></script>
     <script src="./assets/js/fluid.js"></script>
+    <script src="./assets/js/humberger.js"></script>
 </body>
 
 </html>
